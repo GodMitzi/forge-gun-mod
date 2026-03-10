ed47bc6fde24854fa7de21e9388ec5af236b8eab
Ivri Aronson <ivriaronson@gmail.com>
2026-03-10 11:58:35 +0200

    .

--- src/main/java/com/gunmod/GunMod.java
+++ src/main/java/com/gunmod/GunMod.java
@@ -1,12 +1,11 @@
 package com.gunmod;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import org.slf4j.Logger;
 
 @Mod(GunMod.MOD_ID)
@@ -18,15 +17,17 @@ public class GunMod {
         var modEventBus = context.getModBusGroup();
 
         FMLCommonSetupEvent.getBus(modEventBus).addListener(this::commonSetup);
-        MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         LOGGER.info("Gun Mod setup complete!");
     }
 
-    @SubscribeEvent
-    public void onServerStarting(net.minecraftforge.event.server.ServerStartingEvent event) {
-        LOGGER.info("Gun Mod server starting!");
+    @Mod.EventBusSubscriber(modid = MOD_ID)
+    public static class ServerEvents {
+        @SubscribeEvent
+        public static void onServerStarting(ServerStartingEvent event) {
+            LOGGER.info("Gun Mod server starting!");
+        }
     }
 }
